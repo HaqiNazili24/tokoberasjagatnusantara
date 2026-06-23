862f8b2cb97e2906e0642669d2bd01e5bbda00b9
Mohammad Haqi Nazili <nazilihaq1111@gmail.com>
2026-06-23 15:56:24 +0700

    Add SSL support for Aiven MySQL

--- config/database.php
+++ config/database.php
@@ -18,6 +18,9 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => 'InnoDB',
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', '/etc/ssl/certs/ca-certificates.crt'),
+            ]) : [],
         ],
     ],
     'migrations' => 'migrations',
