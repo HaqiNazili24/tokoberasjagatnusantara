c97efe4468fb16febb760ec87caeddffe84f2db2
Mohammad Haqi Nazili <nazilihaq1111@gmail.com>
2026-06-26 18:59:39 +0700

    Configure Paginator to use Bootstrap 5 for correctly sized navigation arrows

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 use App\Models\Cart;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,6 +13,8 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        Paginator::useBootstrapFive();
+
         View::composer('*', function ($view) {
             $cartCount = 0;
             if (auth()->check() && auth()->user()->isCustomer()) {
